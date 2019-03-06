b8dd60acea5057ca65eadaa86dfbd10b26702e95
VNana <1798488508@qq.com>
2019-03-06 16:22:24 +0800

    添加部分页面‘
    git push
    
    MacBook-Pro-2:Unity unity$ cd unite_wechat/
    MacBook-Pro-2:unite_wechat unity$ git add .
    MacBook-Pro-2:unite_wechat unity$ git commit -m add
    [master 053f7ae] add
     13 files changed, 244 insertions(+)
     create mode 100644 app.js
     create mode 100644 app.json
     create mode 100644 app.wxss
     create mode 100644 pages/index/index.js
     create mode 100644 pages/index/index.json
     create mode 100644 pages/index/index.wxml
     create mode 100644 pages/index/index.wxss
     create mode 100644 pages/logs/logs.js
     create mode 100644 pages/logs/logs.json
     create mode 100644 pages/logs/logs.wxml
     create mode 100644 pages/logs/logs.wxss
     create mode 100644 project.config.json
     create mode 100644 utils/util.js
    MacBook-Pro-2:unite_wechat unity$ git push
    Username for https://github.com: 2025506282@qq.com
    Password for https://2025506282@qq.com@github.com:
    Enumerating objects: 20, done.
    Counting objects: 100% (20/20), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (17/17), done.
    Writing objects: 100% (19/19), 3.79 KiB | 1.89 MiB/s, done.
    Enumerating objects: 20, done.
    Counting objects: 100% (20/20), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (17/17), done.
    Writing objects: 100% (19/19), 3.79 KiB | 1.89 MiB/s, done.
    Total 19 (delta 0), reused 0 (delta 0)
    To https://github.com/kkfrank/unite_wechat.git
       8273388..053f7ae  master -> master
    MacBook-Pro-2:unite_wechat unity$ cd /Users/unity/Unity/unite_wechat
    MacBook-Pro-2:unite_wechat unity$ git add .
    MacBook-Pro-2:unite_wechat unity$ git commit -m 添加部分页面‘

---
+++ pages/nav/nav.js
@@ -0,0 +1,89 @@
+// pages/nav/nav.js
+Page({
+  /**
+   * 用户跳转不同的nav
+   */
+  switchNav(e) {
+    wx.navigateTo({
+      url: e.target.dataset.url
+    })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navList:[
+      {
+        name: '评论',
+        url: '../argument/argument'
+      },
+      {
+        name: '赞助商',
+        url: '../support/support'
+      },
+      {
+        name: '场地',
+        url: '../place/place'
+      },
+      {
+        name: '讲师',
+        url: '../teacher/teacher'
+      }
+    ]
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
