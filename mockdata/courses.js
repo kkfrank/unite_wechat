6ffc96576bcc5b58d191d207ea01a8b990869958
frank <360305993@qq.com>
2019-03-19 18:47:07 +0800

    add course

---
+++ mockdata/courses.js
@@ -0,0 +1,61 @@
+var courses = [
+  {
+    id: 1,
+    time: "2019-03-29 9:30-10:00",
+    title: '签到，unity宣传影片赏析',
+    isCourse: false
+  },
+  {
+    id: 2,
+    teacherId:'lwx',
+    time: "2019-03-29 10:00-11:00",
+    title: 'Unity引擎源码解析-ShaderLab原理及优化',
+    teacher: '刘伟贤',
+    isCourse: true
+  },
+  {
+    id: 3,
+    teacherId: 'ly',
+    time: "2019-03-29 11:00-12:00",
+    title: '使用摄影制图法打造3A级人物角色',
+    teacher: '林宇',
+    isCourse: true
+  },
+  {
+    id: 4,
+    time: "2019-03-29 12:00-14:00",
+    title: '午饭+午休',
+    isCourse: false
+  },
+  {
+    id: 5,
+    teacherId: 'ycy',
+    time: "2019-03-29 14:00-15:00",
+    title: '使用Visual Effect Graph实现酷炫的特效',
+    teacher: '殷崇英',
+    isCourse: true
+  },
+  {
+    id: 6,
+    teacherId: 'zlm',
+    time: "2019-03-29 15:00-16:00",
+    title: 'Unity性能优化经验分享',
+    teacher: '张黎明',
+    isCourse: true
+  },
+  {
+    id: 7,
+    teacherId: 'bjy',
+    time: "2019-03-29 16:00-17:00",
+    title: 'Unity 2018.3版本新功能介绍',
+    teacher: '鲍健运',
+    isCourse: true
+  },
+  {
+    id: 8,
+    time: "2019-03-29 14:00-17:20",
+    title: 'Workshop ：项目性能诊断与美术指导',
+    isCourse: false
+  }
+]
+export {courses}
--- pages/agenda/list/list.js
+++ pages/agenda/list/list.js
@@ -1,34 +1,25 @@
 // pages/agenda/list/list.js
+import { courses } from '../../../mockdata/courses.js';
+console.log(courses)
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    list: [{
-      url: 'https://10.url.cn/eth/ajNVdqHZLLCIgUe7QmgPibps3nDVKYMUYKu8Q0yP7qjUMDhm1KXd0JEdO19soD9icicq7Umo5Ua7Cs/',
-      name: '悟空',
-      startTime: '2019-09-02 9:00',
-      endTime: '2019-10-02 11:00',
-      place: '上海交通大学',
-      profession: '高级讲师',
-      course: 'Java',
-      result: '一年店铺销售额两千万上亿企业运营总监',
-      advantage: '搜索优化、直通ada',
-      description: '专业电商从业讲师数据化定制辅导淘宝天猫店铺爆款打造/标题制作/标题优化/手淘首页流量获取/手淘自然搜索流量获取/直通车付费推广/提升直通车质量分/淘宝客推广/生意参谋解析/关键词权重解析/钻展推广/淘宝社区/淘宝直播/活动报名/内容营销/双十一活动淘宝货源/店铺创业网络远程名师在线学习'
-    }, {
-      url: 'https://10.url.cn/eth/ajNVdqHZLLCIgUe7QmgPibps3nDVKYMUYKu8Q0yP7qjUMDhm1KXd0JEdO19soD9icicq7Umo5Ua7Cs/',
-      name: '悟空',
-      startTime: '2019-09-02',
-      endTime: '2019-10-02',
-      place: '上海交通大学',
-      profession: '高级讲师',
-      course: 'C++',
-      result: '一年店铺销售额两千万上亿企业运营总监',
-      advantage: '搜索优化、直通ada',
-      description: '专业电商从业讲师数据化定制辅导淘宝天猫店铺爆款打造/标题制作/标题优化/手淘首页流量获取/手淘自然搜索流量获取/直通车付费推广/提升直通车质量分/淘宝客推广/生意参谋解析/关键词权重解析/钻展推广/淘宝社区/淘宝直播/活动报名/内容营销/双十一活动淘宝货源/店铺创业网络远程名师在线学习'
-    }]
+    courses: courses 
   },
-
+  toDetail:function(ev){
+    console.log(ev.currentTarget.dataset);
+    var item = ev.currentTarget.dataset.item;
+    if(!item  || !item.isCourse){
+      return
+    }
+    var { id, teacherId} = item;
+    console.log(id,teacherId)
+    wx.redirectTo({
+      url: `../detail/detail?id=${id}&teachId=${teacherId}`,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -64,24 +55,8 @@ Page({
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
-  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ 
 })
