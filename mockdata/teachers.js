946d647cd2d4d7e006ed681da52345328c62319c
sunji <2025506282@qq.com>
2019-03-19 18:37:06 +0800

    添加讲师数据

---
+++ mockdata/teachers.js
@@ -0,0 +1,39 @@
+
+const teachers = [
+  {
+    id: 'zlm',
+    url: '/images/zlm.png',
+    name: '张黎明',
+    position: 'Unity大中华区技术总监',
+    description: '现任Unity大中华区技术总监。进入游戏行业超过十年时间，期间参与过游戏引擎研发、Autodesk公司产品的通用图形引擎研发，以及基于商业游戏引擎的项目开发。因热爱游戏而进入游戏行业，经历多年引擎研发，深知一款卓越的游戏引擎对开发者的价值，也致力于让更多的开发者可以用Unity更快的实现自己的愿景。'
+  },
+  {
+    id: 'cl',
+    url: '/images/cl.png',
+    name: '成亮',
+    position: 'Unity大中华区技术经理',
+    description: '从事技术推广工作。具有十余年游戏研发经验，深度使用和优化过多款不同类型的游戏引擎。曾任职于腾讯和育碧，参与了多款热门移动游戏的开发。'
+  },
+  {
+    id: 'ycy',
+    url: '/images/ycy.png',
+    name: '殷崇英',
+    position: 'Unity大中华区技术经理',
+    description: 'American University Game Design硕士，有多年在游戏实验室进行跨领域游戏研发的经验，专注于Unity应用多样性推广。'
+  },
+  {
+    id: 'ly',
+    url: '/images/ly.png',
+    name: '林宇',
+    position: 'Unity大中华区技术美术',
+    description: '在游戏行业工作十多余年，拥有深厚的游戏研发经验。曾负责多款Unity主机，手机及VR游戏的美术制作，精通Unity实时渲染特效制作及高效率性能优化。'
+  },
+  {
+    id: 'lwx',
+    url: '/images/lwx.png',
+    name: '刘伟贤',
+    position: 'Unity大中华区技术支持工程师',
+    description: '曾在多个国内一线游戏研发大厂就职，领导并且参与多款大型MMO的研发，拥有多年的游戏研发经验。精通Unity开发，致力于帮助广大开发者解决Unity项目开发及技术难题。'
+  }
+]
+export default teachers
